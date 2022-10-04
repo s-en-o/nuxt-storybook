5fa4469ab0b9e40b20f228347a42a55c8c1deab9
Seno <borlandseno@yahoo.com>
2022-10-04 22:45:16 +1300

    feat (component): tidy up component
    Tidy up component into their own folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ export default {
     plugins: [],
 
     // Auto import components: https://go.nuxtjs.dev/config-components
-    components: true,
+    components: [{ path: '~/components', extensions: ['vue'] }],
 
     // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: ['@nuxt/postcss8', '@nuxtjs/composition-api/module'],
